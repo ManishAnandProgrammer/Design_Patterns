75cf4d31fd68db4d1f8ae313288f4cc91a41f34e
Manish Anand <manishanandprogrammer@gmail.com>
2021-10-25 18:57:25 +0530

    initial commit

---
+++ src/main/java/com/manish/chain_of_responsibility/Atm.java
@@ -0,0 +1,5 @@
+package com.manish.chain_of_responsibility;
+
+public class Atm {
+
+}
---
+++ src/test/java/com/manish/chain_of_responsibility/AtmTest.java
@@ -0,0 +1,11 @@
+package com.manish.chain_of_responsibility;
+
+import org.junit.jupiter.api.Test;
+
+public class AtmTest {
+
+    @Test
+    void whenUserEnterValueWhichIsNotDivisibleByTenThanReturnException() {
+        Atm atm = new Atm();
+    }
+}
